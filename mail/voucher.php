ee071c66340296f9c85796f39504e725a5c04698
pablopacastell <pablopacastell@gmail.com>
2019-05-07 19:27:29 +0200

    imagen de voucher

--- mail/voucher.php
+++ mail/voucher.php
@@ -23,11 +23,11 @@ $tripDateTo = strip_tags(htmlspecialchars($_POST['tripDateTo']));
 $city = strip_tags(htmlspecialchars($_POST['city']));
 
 // Create the email and send the message
-$to = 'info@yourtravelpic.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'sales@yourtravelpic.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Voucher Form:  $name";
 $email_body = "You have received a new message from your website voucher form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nGroup name: $groupName\n\nCity: $city\n\nDestination: $destination\n\nTrip Date From:\n$tripDateFrom\n\nTrip Date To:\n$tripDateTo";
 
-$headers = "From: recruitment@travelpic.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: sales@travelpic.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
